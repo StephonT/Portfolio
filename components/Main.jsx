7be978a788f258816d33f949d0d9f11224d15025
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-08 18:05:04 -0500

    added styling to main page

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div></div>
+    <div className='w-full h-screen text-center'>
+        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+            <div>
+              <p></p>
+            </div>
+        </div>
+    </div>
   )
 }
 
