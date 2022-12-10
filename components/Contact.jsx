d8775bdcfee37ded15dbb30a6b71d399a131d125
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-12-10 09:44:47 -0500

    added smooth scroll

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -9,7 +9,7 @@ import contactImg from "../public/assets/contactMe.jpg";
 
 const Contact = () => {
   return (
-    <div className="w-full lg:h-screen">
+    <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
         <p className="text-xl tracking-widest uppercase text-white">Contact</p>
         <h2 className="py-4">How To Get In Touch With Me</h2>
